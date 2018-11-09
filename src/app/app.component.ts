8b6472865b09b7ac7a8b7e52b0a0229eb94872aa
Sampuccino <Sampuccino@gmail.com>
2018-11-08 19:34:39 -0800

    Completed first book exercise.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  articles: Article[];
+
+
+  constructor() {
+    this.articles = [
+      new Article('Angular JS', 'http://google.com', 100),
+      new Article('Node JS', 'http://google.com', 33),
+      new Article('Python', 'http://google.com', 8)
+    ];
+  }
+
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
   }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
+
 }
+
